e05ee38ed49115409453e68b2db2c67e53b744a8
Tim600hz <107760546+Tim600hz@users.noreply.github.com>
2024-04-18 14:18:20 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,15 @@ public class Main {
         try {
             File Arquivo = new File("Arquivo.txt");
             System.out.println("Arquivo criado!");
-            FileWriter fileReader = new FileWriter(Arquivo); // A stream that connects to the text file
-            BufferedWriter bufferedWriter = new BufferedWriter(fileReader); // Connect the FileWriter to the BufferedWriter
+            FileWriter fileReader = new FileWriter(Arquivo); 
+            BufferedWriter bufferedWriter = new BufferedWriter(fileReader);
             System.out.println("Programado o escritor de arquivo");
             for (String s : text) {
                 bufferedWriter.write(s + "\n");
             }
             System.out.println(" Arquivo gravado com sucesso");
 
-            bufferedWriter.close (); // Close the stream
+            bufferedWriter.close (); 
         } catch (Exception e) {
             e.printStackTrace();
         }
